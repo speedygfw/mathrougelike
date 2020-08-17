53a00f88162dd224ec47df7b358883914174b940
Frank <fwallenborn@gmx.de>
2020-08-17 19:37:44 +0200

    nn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var app = require('express')();
-var express = require('express');
+import { static } from 'express';
 
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 //app.use(app.static('public'));
-app.use(express.static('public'));
+app.use(static('public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
 
-http.listen(process.env.PORT | 3000, () => {
-  console.log('listening on ' + process.env.PORT | 3000);
+http.listen(process.env.PORT, () => {
+  console.log('listening on ' + process.env.PORT);
 });
 
 
