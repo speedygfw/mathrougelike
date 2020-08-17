f5d4f0b5c037be67e211b319b23b6a98b362d00c
Frank <fwallenborn@gmx.de>
2020-08-17 19:18:53 +0200

    a

--- index.js
+++ index.js
@@ -5,29 +5,22 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 //app.use(app.static('public'));
-//var numUsers=0;
-
 app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
 
-http.listen(process.env.PORT | 80, () => {
-  console.log('listening on ' + (process.env.PORT | 80));
+http.listen(process.env.PORT | 3000, () => {
+  console.log('listening on ' + process.env.PORT | 3000);
 });
 
 
 
 io.on('connection', (socket) => {
-   // ++numUsers;
-    console.log("user connected...");
-    //socket.broadcast.emit('users', numUsers);
-
+    socket.broadcast.emit('new connection')
 
     socket.on('chat message', (msg) => {
-      //console.log(msg);
+      console.log(msg);
       io.emit('chat message', msg);
     });
   });
-
-
