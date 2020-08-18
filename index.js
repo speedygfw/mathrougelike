da3066af18a13fe017dbffd63db4aceb1bf5b9eb
Frank <fwallenborn@gmx.de>
2020-08-18 18:11:07 +0200

    added numOfUsers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var express = require('express');
 
 
+
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
@@ -11,17 +12,28 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
 
-http.listen(process.env.PORT, () => {
-  console.log('listening on ' + process.env.PORT);
+http.listen(process.env.PORT | 80, () => {
+  console.log('listening on ' + (process.env.PORT | 80));
+
 });
 
+var numUsers = 0;
 
 
 io.on('connection', (socket) => {
-    socket.broadcast.emit('new connection')
+  console.log(`Socket ${socket.id} connected.`);
+    ++numUsers;
+    socket.broadcast.emit('numUsers', numUsers);
 
     socket.on('chat message', (msg) => {
       console.log(msg);
       io.emit('chat message', msg);
     });
+
+    socket.on('disconnect', () => {
+      --numUsers;
+      socket.broadcast.emit('numUsers', numUsers);
+      console.log(`Socket ${socket.id} disconnected.`);
+    });
+
   });
